b9c55778514d3b3cf6fdf29fbf32fb814ba99770
TJ Sherrill <tj@ravennainteractive.com>
2011-12-29 14:32:00 -0800

    fixed questions migrations

--- db/migrate/20111229175447_create_questions.rb
+++ db/migrate/20111229175447_create_questions.rb
@@ -1,11 +1,13 @@
 class CreateQuestions < ActiveRecord::Migration
   def up
+      create_table :questions
       add_column :questions, :name, :string
       add_column :questions, :email, :string
       add_column :questions, :question, :text
   end
   
   def down
+    drop_table :questions
     remove_column :questions, :name, :string
     remove_column :questions, :email, :string
     remove_column :questions, :question, :text
