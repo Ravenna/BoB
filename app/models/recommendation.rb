7245d68cd78d180f08242a81d7ea8a83dbd29d06
TJ Sherrill <tj@ravennainteractive.com>
2012-03-09 14:25:06 -0800

    delete working on Upload1

--- app/models/recommendation.rb
+++ app/models/recommendation.rb
@@ -3,8 +3,11 @@ class Recommendation < ActiveRecord::Base
   belongs_to :user
   has_many :approvals, :dependent => :destroy
   accepts_nested_attributes_for :approvals, :allow_destroy => true, :reject_if => :all_blank
-  attr_accessible :nominee, :award_id, :approvals_attributes, :title, :department, :summary, :accomplishments, :caption, :url, :supervisor, :upload, :upload2, :upload3
+  attr_accessible :nominee, :award_id, :approvals_attributes, :title, :department, :summary, :accomplishments, :caption, :url, :supervisor, :upload, :delete_upload, :upload2, :upload3
   validates  :nominee, :title, :department, :summary, :accomplishments, :supervisor,  :presence => true
+  
+  before_validation :clear_upload
+  
   
   has_attached_file :upload,
     :storage => :s3,
@@ -27,5 +30,16 @@ class Recommendation < ActiveRecord::Base
          :access_key_id => ENV['MyAccessKey'],
           :secret_access_key => ENV['MySecretKey']
        }
-  
+   def delete_upload=(value)
+      @delete_upload = !value.to_i.zero?
+    end
+
+    def delete_upload
+      !!@delete_upload
+    end
+    alias_method :delete_upload?, :delete_upload
+    
+    def clear_upload
+      self.upload = nil if delete_upload? && !upload.dirty?
+    end
 end
