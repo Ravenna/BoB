a55a08ca5d43a9346afe96ea8e2a054de382f42a
TJ Sherrill <tj@ravennainteractive.com>
2014-12-23 12:24:08 -0800

    update employee edit and banner

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,7 +5,7 @@ class User < ActiveRecord::Base
   devise :database_authenticatable, :registerable, :recoverable, :rememberable, :trackable, :confirmable
 
   # Setup accessible (or protected) attributes for your model
-  attr_accessible :email, :password, :password_confirmation, :remember_me, :name, :smt, :admin
+  attr_accessible :email, :first_name, :last_name, :password, :password_confirmation, :remember_me, :name, :smt, :admin
   
   validates  :email, uniqueness: true
   
